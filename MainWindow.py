c36b5ece047172adf417f379b12272e47053a4f2
mjord <matthewbjordan@gmail.com>
2017-01-30 23:32:10 +0100

    Add files via upload

--- MainWindow.py
+++ MainWindow.py
@@ -1,12 +1,15 @@
 from database_manager import Database
+import sqlite3 as sql
 import sys
 import os
+import re
 
 os.environ['QT_API'] = 'pyqt5'
 
 from PyQt5 import uic
 from PyQt5 import QtWidgets
 from PyQt5 import QtGui
+from PyQt5 import QtCore
 # ipython won't work if this is not correctly installed. And the error message will be misleading
 from PyQt5 import QtSvg
 # from PyQt5.QtCore import QTimer, QSize
@@ -28,17 +31,13 @@ class MainWindow(QtWidgets.QMainWindow):
         # self.db = None
         #
         # # Connect up the buttons.
-        # self.actionOpen_Database.triggered.connect(self.open_db)
-        # self.actionAdd_Movie.triggered.connect(self.add_item)
-        # self.actionNew_Database.triggered.connect(self.new_db)
-        # self.iconSize.valueChanged.connect(self.rescale)
-        #
-        # # self.listWidget.itemDoubleClicked.connect(self.play)
-        # # self.listWidget.itemClicked.connect(self.testMovieInfo)
-        #
-        # self.listWidget.itemDoubleClicked.connect(self.twoclick)
-        # # self.listWidget.itemClicked.connect(self.clicked)
-        # self.listWidget.itemClicked.connect(self.oneclick)
+        self.actionOpen_Database.triggered.connect(self.open_db)
+        self.actionProject.triggered.connect(self.new_project)
+        self.actionProcess.triggered.connect(self.new_process)
+        self.actionSample.triggered.connect(self.new_wafer)
+
+
+
         #
         # # self.searchText.textChanged.connect(self.update_search)
         # self.searchText.editingFinished.connect(self.update_list)
@@ -46,11 +45,248 @@ class MainWindow(QtWidgets.QMainWindow):
         #
         # self.timer = None
 
-        ipyConsole = QIPythonWidget(customBanner="Welcome to the embedded ipython console\n")
-        self.gridLayout.addWidget(ipyConsole)
+        # self.ipyConsole = QIPythonWidget(customBanner="Welcome to the embedded ipython console\n")
+        # self.gridLayout.addWidget(self.ipyConsole)
 
         self.show()
 
+    def open_db(self):
+        names = QtWidgets.QFileDialog.getOpenFileName(self, 'Open database', '','Open database (*.sqlite)')
+
+        if names:
+            fname = names[0]
+            if len(fname) <= 7:
+                fname = fname + ".sqlite"
+            elif fname[-7:] != ".sqlite":
+                fname = fname + ".sqlite"
+
+            sql.connect(fname)
+
+            self.db = Database(db_name=fname)
+
+    def new_project(self):
+        self.add_window = NewProject(self.db)
+        # self.add_window = NewProject(self.db, rowid=2)
+        self.add_window.show()
+
+    def new_process(self):
+        self.add_window = NewProcess(self.db)
+        # self.add_window = NewProject(self.db, rowid=2)
+        self.add_window.show()
+
+    def new_wafer(self):
+        self.add_window = NewWafer(self.db)
+        # self.add_window = NewProject(self.db, rowid=2)
+        self.add_window.show()
+
+
+class NewProject(QtWidgets.QWidget):
+    def __init__(self, db, **kwargs):
+        from datetime import date, datetime
+
+
+        self.db = db
+        if "rowid" in kwargs:
+            rowid = kwargs.get("rowid")
+            if isinstance(rowid, int):
+                self.rowid = rowid
+            else:
+                self.rowid = None
+        else:
+            self.rowid = None
+
+        super().__init__()
+        self.ui = "make_project_gui.ui"  # Enter file here.
+
+        # Set up the user interface from Designer.
+        uic.loadUi(self.ui, self)
+
+
+
+        if self.rowid is None:
+            self.makeButton.clicked.connect(self.make_project)
+            today = date.today()
+            try:
+                tenyears =  today.replace(year=today.year + 10)
+            except ValueError:
+                tenyears = today + (date(today.year + 10, 1, 1) - date(today.year, 1, 1))
+
+            qtDate1 = QtCore.QDate.fromString(str(today), 'yyyy-MM-dd')
+            qtDate2 = QtCore.QDate.fromString(str(tenyears), 'yyyy-MM-dd')
+            self.startDate.setDate(qtDate1)
+            self.endDate.setDate(qtDate2)
+        else:
+            q = "SELECT * FROM PROJECT WHERE ROWID = %i"%self.rowid
+            rows = self.db.query(q)
+            row = rows[0]
+            qtDate1 = QtCore.QDate.fromString(row["DATE_START"][:-9], 'yyyy-MM-dd')
+            qtDate2 = QtCore.QDate.fromString(row["DATE_END"][:-9], 'yyyy-MM-dd')
+            self.startDate.setDate(qtDate1)
+            self.endDate.setDate(qtDate2)
+
+            self.nameText.setText(row["NAME"])
+            self.overviewText.setText(row["OVERVIEW"])
+
+            self.makeButton.setText("Edit Project")
+            self.makeButton.clicked.connect(self.edit_project)
+
+    def make_project(self):
+        name = self.nameText.text()
+        if name == "":
+            self.nameText.setStyleSheet("color: rgb(255, 0, 0);")
+            self.nameText.setText("Must supply a Name")
+            return
+
+        overview = self.overviewText.toPlainText()
+
+        name = re.sub("'", "", name)
+        overview = re.sub("'", "", overview)
+
+
+        qstart = self.startDate.date()
+        start = qstart.toString('yyyy-MM-dd') + ' 12:00:00'
+        qend = self.endDate.date()
+        end = qend.toString('yyyy-MM-dd') + ' 12:00:00'
+
+        q = "INSERT INTO PROJECT (NAME, OVERVIEW, DATE_START, DATE_END) VALUES ('%s', '%s', '%s', '%s')"%(name, overview, start, end)
+        self.db.query(q)
+        self.close()
+
+    def edit_project(self):
+        name = self.nameText.text()
+        if name == "":
+            self.nameText.setStyleSheet("color: rgb(255, 0, 0);")
+            self.nameText.setText("Must supply a Name")
+            return
+
+        overview = self.overviewText.toPlainText()
+
+        name = re.sub("'", "", name)
+        overview = re.sub("'", "", overview)
+
+        qstart = self.startDate.date()
+        start = qstart.toString('yyyy-MM-dd') + ' 12:00:00'
+        qend = self.endDate.date()
+        end = qend.toString('yyyy-MM-dd') + ' 12:00:00'
+
+        q = "UPDATE PROJECT SET NAME = '%s', OVERVIEW = '%s', DATE_START = '%s', DATE_END = '%s' WHERE ROWID = %i" % (
+        name, overview, start, end, self.rowid)
+        self.db.query(q)
+        self.close()
+
+class NewProcess(QtWidgets.QWidget):
+    def __init__(self, db, **kwargs):
+        from datetime import date, datetime
+
+
+        self.db = db
+        if "rowid" in kwargs:
+            rowid = kwargs.get("rowid")
+            if isinstance(rowid, int):
+                self.rowid = rowid
+            else:
+                self.rowid = None
+        else:
+            self.rowid = None
+
+        super().__init__()
+        self.ui = "new_process_gui.ui"  # Enter file here.
+
+        # Set up the user interface from Designer.
+        uic.loadUi(self.ui, self)
+
+
+
+        if self.rowid is None:
+            self.makeButton.clicked.connect(self.make_process)
+
+        else:
+            q = "SELECT * FROM PROCESS_STEP WHERE ROWID = %i"%self.rowid
+            rows = self.db.query(q)
+            row = rows[0]
+
+            self.nameText.setText(row["NAME"])
+            self.overviewText.setText(row["OVERVIEW"])
+
+            self.makeButton.setText("Edit Project")
+            self.makeButton.clicked.connect(self.edit_process)
+
+    def make_process(self):
+        name = self.nameText.text()
+        if name == "":
+            self.nameText.setStyleSheet("color: rgb(255, 0, 0);")
+            self.nameText.setText("Must supply a Name")
+            return
+
+        overview = self.overviewText.toPlainText()
+
+        name = re.sub("'", "", name)
+        overview = re.sub("'", "", overview)
+
+
+        q = "INSERT INTO PROCESS_STEP (NAME, OVERVIEW) VALUES ('%s', '%s')"%(name, overview)
+        self.db.query(q)
+        self.close()
+
+    def edit_process(self):
+        name = self.nameText.text()
+        if name == "":
+            self.nameText.setStyleSheet("color: rgb(255, 0, 0);")
+            self.nameText.setText("Must supply a Name")
+            return
+
+        overview = self.overviewText.toPlainText()
+
+        name = re.sub("'", "", name)
+        overview = re.sub("'", "", overview)
+
+
+        q = "UPDATE PROCESS_STEP SET NAME = '%s', OVERVIEW = '%s' WHERE ROWID = %i" % (name, overview, self.rowid)
+        self.db.query(q)
+        self.close()
+
+class NewWafer(QtWidgets.QWidget):
+    def __init__(self, db, **kwargs):
+        from datetime import date, datetime
+
+
+        self.db = db
+        if "rowid" in kwargs:
+            rowid = kwargs.get("rowid")
+            if isinstance(rowid, int):
+                self.rowid = rowid
+            else:
+                self.rowid = None
+        else:
+            self.rowid = None
+
+        super().__init__()
+        self.ui = "new_wafer_gui.ui"  # Enter file here.
+
+        # Set up the user interface from Designer.
+        uic.loadUi(self.ui, self)
+
+
+
+        if self.rowid is None:
+            self.makeButton.clicked.connect(self.make_wafer)
+
+        else:
+            q = "SELECT * FROM PROCESS_STEP WHERE ROWID = %i"%self.rowid
+            rows = self.db.query(q)
+            row = rows[0]
+
+            self.nameText.setText(row["NAME"])
+            self.notesText.setText(row["OVERVIEW"])
+
+            self.makeButton.setText("Edit Project")
+            self.makeButton.clicked.connect(self.edit_wafer)
+
+    def make_wafer(self):
+        self.close()
+
+    def edit_wafer(self):
+        self.close()
 
 
 
@@ -94,22 +330,22 @@ class QIPythonWidget(RichJupyterWidget):
         self._execute(command,False)
 
 
-class ExampleWidget(QtWidgets.QMainWindow):
-    """ Main GUI Window including a button and IPython Console widget inside vertical layout """
-    def __init__(self, parent=None):
-        super(ExampleWidget, self).__init__(parent)
-        self.setWindowTitle('iPython in PyQt5 app example')
-        self.mainWidget = QtWidgets.QWidget(self)
-        self.setCentralWidget(self.mainWidget)
-        layout = QtWidgets.QVBoxLayout(self.mainWidget)
-        self.button = QtWidgets.QPushButton('Another widget')
-        ipyConsole = QIPythonWidget(customBanner="Welcome to the embedded ipython console\n")
-        layout.addWidget(self.button)
-        layout.addWidget(ipyConsole)
-        # This allows the variable foo and method print_process_id to be accessed from the ipython console
-        ipyConsole.pushVariables({"foo":43,"print_process_id":print_process_id})
-        ipyConsole.printText("The variable 'foo' and the method 'print_process_id()' are available. Use the 'whos' command for information.\n\nTo push variables run this before starting the UI:\n ipyConsole.pushVariables({\"foo\":43,\"print_process_id\":print_process_id})")
-        self.setGeometry(300, 300, 800, 600)
+# class ExampleWidget(QtWidgets.QMainWindow):
+#     """ Main GUI Window including a button and IPython Console widget inside vertical layout """
+#     def __init__(self, parent=None):
+#         super(ExampleWidget, self).__init__(parent)
+#         self.setWindowTitle('iPython in PyQt5 app example')
+#         self.mainWidget = QtWidgets.QWidget(self)
+#         self.setCentralWidget(self.mainWidget)
+#         layout = QtWidgets.QVBoxLayout(self.mainWidget)
+#         self.button = QtWidgets.QPushButton('Another widget')
+#         ipyConsole = QIPythonWidget(customBanner="Welcome to the embedded ipython console\n")
+#         layout.addWidget(self.button)
+#         layout.addWidget(ipyConsole)
+#         # This allows the variable foo and method print_process_id to be accessed from the ipython console
+#         ipyConsole.pushVariables({"foo":43,"print_process_id":print_process_id})
+#         ipyConsole.printText("The variable 'foo' and the method 'print_process_id()' are available. Use the 'whos' command for information.\n\nTo push variables run this before starting the UI:\n ipyConsole.pushVariables({\"foo\":43,\"print_process_id\":print_process_id})")
+#         self.setGeometry(300, 300, 800, 600)
 
 def print_process_id():
     print('Process ID is:', os.getpid())
